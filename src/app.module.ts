52cf56ba0c48d408e56420ab060f6f24e46071ec
SolForte <mateusbrandao1234@gmail.com>
2023-08-28 10:00:34 -0300

    chore: prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
